f6e3fdb12303bf3cf2fb4a4b247aeb882a2c3bea
Elliot Chapple <47708825+lerryberry@users.noreply.github.com>
2023-02-16 20:48:09 +1100

    Update backupStream.sh

--- backupStream.sh
+++ backupStream.sh
@@ -12,5 +12,5 @@ ffmpeg \
                 -loop 1 -r 18 \
                 -i $imageName \
                 -vcodec libx264 -crf 10 -pix_fmt yuv420p \
-        -f flv rtmp://a.rtmp.youtube.com/live2/3sa2-heh8-0yhb-tgcz-41ef
+        -f flv rtmp://a.rtmp.youtube.com/live2/<code>
 )
