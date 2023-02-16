4c3c42fea54d32d0c0da61e3277ed11456d474db
Elliot Chapple <47708825+lerryberry@users.noreply.github.com>
2023-02-16 18:51:44 +1100

    Update backupStream.sh

--- backupStream.sh
+++ backupStream.sh
@@ -1,14 +1,16 @@
 placeholderURL="https://raw.githubusercontent.com/lerryberry/ChickenCamBackup/main/placeholder-offline.jpg"
 imageName=$(basename $placeholderURL)
-$(curl $placeholder > $imageName)
+$(curl $placeholderURL > $imageName)
 $(pkill "ffmpeg")
-$(sleep 2)
+$(sleep 8)
 
 $(
-  ffmpeg -loop 1 -framerate 30 \
-    -f lavfi \
-      -i anullsrc c:a aac \
-    -i $imageName \
-      -c:v libx264 -pix_fmt yuv420p \
-  -f flv rtmp://a.rtmp.youtube.com/live2/3sa2-heh8-0yhb-tgcz-41ef
+ffmpeg \
+        -f lavfi \
+                -i anullsrc \
+        -f image2 \
+                -loop 1 -r 18 \
+                -i $imageName \
+                -vcodec libx264 -crf 10 -pix_fmt yuv420p \
+        -f flv rtmp://a.rtmp.youtube.com/live2/3sa2-heh8-0yhb-tgcz-41ef
 )
